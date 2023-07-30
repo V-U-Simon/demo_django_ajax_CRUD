61aca53cbf56bf92eadd19f16909f6ca482fcd41
V-U-Simon <V-U-Simon@yandex.ru>
2023-07-30 22:41:23 +0300

    crud: migrate & urls

--- config/todo_crud_app/urls.py
+++ config/todo_crud_app/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from .apps import TodoCrudAppConfig
+
+app_name = TodoCrudAppConfig.name
 
 urlpatterns = [
     # path('', include('todo_crud_app.urls'), namespace='crud'),
