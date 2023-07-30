d874f1475e4a508fae53acff4fb84bcd00493a5c
V-U-Simon <V-U-Simon@yandex.ru>
2023-07-30 22:45:12 +0300

    crud: view & urls

--- config/todo_crud_app/urls.py
+++ config/todo_crud_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .apps import TodoCrudAppConfig
+from . import views
 
 app_name = TodoCrudAppConfig.name
 
 urlpatterns = [
-    # path('', include('todo_crud_app.urls'), namespace='crud'),
+    path('', views.TaskListView.as_view(), name='task_list'),
 ]
