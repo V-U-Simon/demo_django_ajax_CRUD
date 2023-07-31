1f2f482d7fd84b70b4f71656c5b15b6552d1c112
V-U-Simon <V-U-Simon@yandex.ru>
2023-07-31 19:18:05 +0300

    crud: ajax get

--- config/todo_crud_app/urls.py
+++ config/todo_crud_app/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = TodoCrudAppConfig.name
 
 urlpatterns = [
-    path('', views.TaskView.as_view(), name='ajax_crud'),
+    path('', views.TaskView.as_view(), name='home'),
+    path('task/', views.AjaxTaskView.as_view(), name='tasks'),
+    path('task/<int:id>', views.AjaxTaskView.as_view(), name='task'),
 ]
--- config/todo_crud_app/views.py
+++ config/todo_crud_app/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.views import generic
+from django.http import HttpResponseBadRequest, JsonResponse
 
 from .models import Task
 
 
 class TaskView(generic.TemplateView):
     template_name = 'todo_crud_app/task_ajax.html'
+
+
+class AjaxTaskView(generic.View):
+
+    def get(self, request):
+        if request.accepts('application/json'):
+            tasks = list(Task.objects.all().values('title', 'complited'))
+            return JsonResponse({'context': tasks})
+
+    def post(self, request):
+        pass
+
+    def put(self, request):
+        pass
+
+    def delte(self, request):
+        pass
